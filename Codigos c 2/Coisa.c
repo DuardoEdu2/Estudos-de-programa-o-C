de8d5362b6b48392c0148058329fe41eb7353a3e
Eduardo Abrantes <eduardopabrantes@gmail.com>
2023-09-25 07:45:52 -0300

    arrays

--- Codigos c 2/Coisa.c
+++ Codigos c 2/Coisa.c
@@ -6,6 +6,16 @@
 
 int main (){
 
-printf("Fala galera");
+    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0, 30.0};
+
+    printf("%d bytes\n", sizeof(prices));
+
+
+    for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++)
+    {
+        printf("%.2lf, ", prices[i]);
+    }
+
+    return 0;
 
 }
